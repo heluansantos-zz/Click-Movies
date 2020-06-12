dde14182c29feb44362027711d65aee38c150202
heluansantos <heluan.caetano@gmail.com>
2020-06-12 02:49:49 -0300

    Click Movies App

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
+import "react-native-gesture-handler";
 import React from "react";
 import Routes from "./src/routes";
-import "react-native-gesture-handler";
 
 export default function App() {
   return <Routes />;
